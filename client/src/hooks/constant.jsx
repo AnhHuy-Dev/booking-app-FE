f23f7775511dd0be3ef43e99d18288f015a4e045
anhhuy <phamanhhuy22@gmail.com>
2023-06-07 07:13:33 +0700

    upload

--- client/src/hooks/constant.jsx
+++ client/src/hooks/constant.jsx
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.NODE_ENV !== "production" ? "http://localhost:8000/api" : "https://mern-learnit-h16k.onrender.com/api";
+export const apiUrl = process.env.NODE_ENV !== "production" ? "http://localhost:8000/api" : "https://booking-app-be-brown.vercel.app/api";
 
 export const LOCAL_STORAGE_ACCESSTOKEN = "mytoken";
